1bebebfcb2ab9e7cf4c2107c172fe5f5c9969385
HKSFC\devltwchan1 <ltwchan1@sfc.hk>
2024-02-16 09:03:27 +0800

    Lab 3 fix bug

--- chatbot.py
+++ chatbot.py
@@ -11,7 +11,7 @@ def main():
     # Load your token and create an Updater for your Bot
     config = configparser.ConfigParser()
     config.read('config.ini')
-    updater = Updater(token=(config['TELEGRAM']['ACCESS_TOKEN']), request_kwargs={'proxy_url': 'http://ltwchan1:Passjan2021@proxy:8080'},use_context=True)
+    updater = Updater(token=(config['TELEGRAM']['ACCESS_TOKEN']),use_context=True)
     dispatcher = updater.dispatcher
     # You can set this logging module, so you will know when and why things do not work a
     logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',level=logging.DEBUG)
